b57642b300247fa413bb17a42e05304ce86ea71c
Kevin Griffin <kevin@solliance.net>
2022-01-05 13:48:30 -0500

    updates

--- Examples/01_DesignForSpeed/Caching/Program.cs
+++ Examples/01_DesignForSpeed/Caching/Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddStackExchangeRedisCache(options =>
  {
      // Need Redis?  docker run --rm --name redis -p 32768:6379 -d redis redis-server --appendonly yes
      options.Configuration = "localhost:32768,defaultDatabase=5";
-
      options.InstanceName = "demo:"; // appended to all keys
  });
 
